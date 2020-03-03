80f019645ce58969654416c08e104c8d3f1bcc8c
jdburnham <jburnham@stu.southtexascollege.edu>
2020-03-03 16:06:02 -0600

    Added SuperHero API project

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,6 +5,7 @@ import inventory from '../assets/projects/04.png'
 import vuetodo from '../assets/projects/05.png'
 import mynotes from '../assets/projects/06.png'
 import login from '../assets/projects/07.png'
+import superheroes from '../assets/projects/08.png'
 
 const projects = [
   {
@@ -65,6 +66,14 @@ const projects = [
       'vue-router'
     ],
     url: 'https://thelogin.jburnhamprojects.xyz/'
+  },
+  {
+    name: 'Super Heroes API',
+    description:
+      'A Vue.js Application with Vuex, implementing a Super Heroes API. ',
+    image: superheroes,
+    hashtags: ['api', 'vuejs', 'javascript', 'tailwind', 'vuex', 'vue-router'],
+    url: 'https://superheroes.jburnhamprojects.xyz/'
   }
 ]
 
